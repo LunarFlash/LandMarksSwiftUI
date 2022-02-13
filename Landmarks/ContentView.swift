a5443b4aa63b930598c6251a6b7a9ffc59e2ffc8
Yi Wang <logins.yiwang@gmail.com>
2022-02-13 00:15:40 -0500

    add url

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -3,14 +3,22 @@
 //  Landmarks
 //
 //  Created by Yi Wang on 2/12/22.
-//
+// https://developer.apple.com/tutorials/swiftui/creating-and-combining-views
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+            }
+        }.padding()
     }
 }
 
