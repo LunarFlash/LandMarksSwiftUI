2976929299af8d64088bf8bc4de3cdb36454facd
Yi Wang <logins.yiwang@gmail.com>
2022-02-13 14:29:10 -0500

    reorg files

---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,30 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Yi Wang on 2/13/22.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError("Couldn't find \(filename) in main bundle")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Coudn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        return try JSONDecoder().decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
